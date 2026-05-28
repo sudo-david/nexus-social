034c9248aa96c296d3a1963d104c0a6eaf27e0d6
sudo-david <david11duquevelez@outlook.com>
2026-05-28 12:43:21 -0500

    Se añadio los scripts de la db y el modelo ER

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config:
 
     DB_HOST     = os.getenv("DB_HOST", "localhost")
     DB_USER     = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "david1011")
     DB_NAME     = os.getenv("DB_NAME", "red_social")
     DB_PORT     = int(os.getenv("DB_PORT", 3306))
